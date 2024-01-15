da1d5b314bbe324a4f52749add9f2a3e669bcc8d
Anthony <antc615@gmail.com>
2024-01-15 15:16:46 -0600

    add porject structure

--- GoLuckyCoreAPI/settings.py
+++ GoLuckyCoreAPI/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-AUTH_USER_MODEL = 'core.User'
+AUTH_USER_MODEL = 'users.User'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
-    'core',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
--- GoLuckyCoreAPI/urls.py
+++ GoLuckyCoreAPI/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/', include('core.urls')),  # Adjust the path as needed
+    path('api/', include('apps.users.urls')),  # Adjust the path as needed
 ]
