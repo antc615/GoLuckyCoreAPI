bbfe4fd78f4907b1590257e0a78f617ffdddbc96
Anthony <antc615@gmail.com>
2024-01-15 15:29:25 -0600

    migrate to postgres backend

--- GoLuckyCoreAPI/settings.py
+++ GoLuckyCoreAPI/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = "GoLuckyCoreAPI.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'golucky',
+        'USER': 'dev',
+        'PASSWORD': 'dev123',
+        'HOST': 'localhost',  # Set to empty string for localhost
+        'PORT': '',           # Set to empty string for default
     }
 }
 
