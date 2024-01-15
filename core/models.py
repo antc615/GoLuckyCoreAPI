3e5ead08b381a3defeed34f411e8cf008581f16c
Anthony <antc615@gmail.com>
2024-01-15 00:01:15 -0600

    more project init

--- core/models.py
+++ core/models.py
@@ -1,6 +1,4 @@
-from django.db import models
-
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass  # Extend the user model with additional fields if needed
+    pass  # You can add additional fields later
