dc232d40d5d083326c1b3a3ec053a19429332a9d
Anthony <antc615@gmail.com>
2024-01-20 15:43:38 -0600

    initialize chat app, add casssandra config for chat messages

--- GoLuckyCoreAPI/settings.py
+++ GoLuckyCoreAPI/settings.py
@@ -93,6 +93,18 @@ DATABASES = {
         'PASSWORD': 'dev123',
         'HOST': 'localhost',  # Set to empty string for localhost
         'PORT': '',           # Set to empty string for default
+    },
+    'cassandra': {
+        'ENGINE': 'django_cassandra_engine',
+        'NAME': 'golucky',
+        'TEST_NAME': 'test_golucky',
+        'HOST': '127.0.0.1',  # Or your Cassandra cluster address
+        'OPTIONS': {
+            'replication': {
+                'strategy_class': 'SimpleStrategy',
+                'replication_factor': 1
+            }
+        }
     }
 }
 
