674b97a630a58028d45d3a2c2697f76607bf7109
Anthony <antc615@gmail.com>
2024-01-26 21:41:48 -0600

    create atomic transaction for postgres notification aync insert

--- apps/interactions/tasks.py
+++ apps/interactions/tasks.py
@@ -4,6 +4,7 @@ from celery import shared_task
 from utils.cassandra_util import log_notification_event
 from apps.notifications.models import Notification  # Import the Notification model
 import logging
+from django.db import transaction
 
 logger = logging.getLogger(__name__)
 
@@ -13,16 +14,17 @@ def async_log_notification_event(recipient_id, sender_id, event_type, image_id=N
     Logs a notification event to Cassandra and creates a notification in PostgreSQL.
     """
     try:
-        log_notification_event(recipient_id, sender_id, event_type, image_id, message)
 
-        # Create PostgreSQL Notification
-        Notification.objects.create(
-            recipient_id=recipient_id,
-            sender_id=sender_id,
-            notification_type=event_type,
-            image_id=image_id,
-            message=message
-        )
+        with transaction.atomic():
+            # Create PostgreSQL Notification
+            Notification.objects.create(
+                recipient_id=recipient_id,
+                sender_id=sender_id,
+                notification_type=event_type,
+                image_id=image_id,
+                message=message
+            )
+        log_notification_event(recipient_id, sender_id, event_type, image_id, message)
     except Exception as e:
         # Log the exception for debugging purposes
         logger.error(f"Failed to log notification event: {e}")
