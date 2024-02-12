2e14bc4b516bbd7397392be2d4787fc9fe94d213
Anthony <antc615@gmail.com>
2024-02-11 22:52:32 -0600

    fix absolute urls

--- utils/media_util.py
+++ utils/media_util.py
@@ -2,5 +2,5 @@ from django.conf import settings
 
 def build_absolute_image_url(request, image_field):
     if image_field and hasattr(image_field, 'url'):
-        return request.build_absolute_uri(settings.MEDIA_URL + image_field.url)
+        return request.build_absolute_uri(image_field.url)
     return None
